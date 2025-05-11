00618fc0256d5cd47975709e8c7559b9872a883d
davalex2003 <pupkinvasek200356@gmail.com>
2025-05-11 20:37:49 +0300

    add status to assignments list handler

--- src/services/assignment.py
+++ src/services/assignment.py
@@ -66,10 +66,33 @@ class AssignmentService():
             return None
         if data[0][0] not in [STUDENT, TEACHER] or not data[0][5]:
             return None
-        data = self.assignment_repository.get_assignments(course_id)
+        course_assignments = self.assignment_repository.get_assignments(course_id)
         response: List[Assignment] = []
-        for i in data:
-            response.append(Assignment(id=str(i[0]), name=i[1], started_at=i[2], ended_at=i[3]))
+        if data[0][0] == TEACHER:
+            for i in course_assignments:
+                response.append(Assignment(id=str(i[0]), name=i[1], started_at=i[2], ended_at=i[3]))
+            return response
+        for course_assignment in course_assignments:
+            if datetime.now() >= course_assignment[2] and (not course_assignment[3] or datetime.now() < course_assignment[3]):
+                assignment_answers = self.answer_repository.get_answers(data[0][6], course_assignment[0])
+                if not assignment_answers:
+                    response.append(Assignment(id=str(course_assignment[0]), name=course_assignment[1], 
+                                                            started_at=course_assignment[2], ended_at=course_assignment[3],
+                                                            status=PENDING))
+                else:
+                    has_assessment = False
+                    for assignment_answer in assignment_answers:
+                        if assignment_answer[5]:
+                            has_assessment = True
+                            response.append(Assignment(id=str(course_assignment[0]), name=course_assignment[1], 
+                                                            started_at=course_assignment[2], ended_at=course_assignment[3],
+                                                            status=GRADED))
+                    if not has_assessment:
+                        response.append(Assignment(id=str(course_assignment[0]), name=course_assignment[1], 
+                                                        started_at=course_assignment[2], ended_at=course_assignment[3],
+                                                        status=IN_REVIEW))
+            else:
+                continue
         return response
 
     def get_homeworks(self, token: str) -> Optional[List[AssignmentCourse]]:
